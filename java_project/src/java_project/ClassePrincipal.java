e00f79233a07c1f23b68b7a9b281519c7f6adca1
pedrogitkk <pedro.tristao@etec.sp.gov.br>
2025-03-11 12:25:23 -0300

    third

---
+++ java_project/src/java_project/ClassePrincipal.java
@@ -0,0 +1,19 @@
+package java_project;
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("pulando");
+		
+		System.out.printf("Sem pular linha");
+		
+		
+		System.out.prinln("Meu nome é \"fulano\ "");
+				
+		System.out.prinln("Meu nome é \'fulano\ '");
+		
+		System.out.prinln("Meu nome é \n fulano");
+	}
+
+}
